1becd4b1b9e1a4d5cac0c3029d64a0eba2e4f3f6
Subham Satapathy <satapathypro@gmail.com>
2026-06-08 06:14:46 +0530

    Updates

---
+++ _next/static/6b2YER3Zo7ErsBzIbCsbF/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fcontact","\u002Feducation","\u002Fexperience","\u002Fgithub","\u002Fprojects","\u002Fresume","\u002Fsettings"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
